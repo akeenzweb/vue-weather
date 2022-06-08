ad3670aa6ca818229784e9f6d3d22912034e9d3b
akeenzweb <akinboladesalako@gmail.com>
2022-06-08 12:32:20 +0100

    installed axios

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@
 
 <script>
 
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -39,15 +40,20 @@ export default {
   methods: {
     fetchweather (e) {
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-        .then(res => {
-          return res.json();
-        }).then(this.setResults);
+        // fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        // .then(res => {
+        //   return res.json();
+        // }).then(this.setResults);
+        axios.get(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then((response) => {
+          console.log(response.data)
+          this.weather = response.data
+        })
       }
     },
-    setResults (results) {
-      this.weather = results;
-    },
+    // setResults (results) {
+    //   this.weather = results;
+    // },
     dateBuilder () {
       let d = new Date();
       let months = ["January", "Fabuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
