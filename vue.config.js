57e62aad97c5f475950d3e120dafaeb04cbcf0a9
akeenzweb <akinboladesalako@gmail.com>
2022-06-08 11:11:49 +0100

    second commit

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/weatherapp/" : "/",
+};
+
+
+
+
+// module.exports = {
+//   publicPath: ''
+// }
+
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+
+
+ 
